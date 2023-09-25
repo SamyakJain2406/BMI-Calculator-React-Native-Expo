825535d8043de1472238232c51aba83b6605cfa4
SAMYAK JAIN <123292041+SamyakJain2406@users.noreply.github.com>
2023-09-25 23:45:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import bmicalculator from './src';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   
+  },
+});
